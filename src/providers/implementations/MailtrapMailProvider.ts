9a731d29dcf622abf397a3b63ef942c56f5f508d
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-04-03 04:17:57 +0044

    Mailtrap implementation

---
+++ src/providers/implementations/MailtrapMailProvider.ts
@@ -0,0 +1,33 @@
+import Mail from "nodemailer/lib/mailer";
+import { IMailProvider, IMessage } from "../IMailProvider";
+import nodemailer from "nodemailer";
+
+export class MailtrapMailProvider implements IMailProvider {
+  private transporter: Mail;
+
+  constructor() {
+    this.transporter = nodemailer.createTransport({
+      host: "sandbox.smtp.mailtrap.io",
+      port: 2525,
+      auth: {
+        user: "239e181344642a",
+        pass: "7e42110ff01138",
+      },
+    });
+  }
+
+  async sendMail(message: IMessage): Promise<void> {
+    await this.transporter.sendMail({
+      to: {
+        name: message.to.name,
+        address: message.to.email,
+      },
+      from: {
+        name: message.from.name,
+        address: message.from.email,
+      },
+      subject: message.subject,
+      html: message.body,
+    });
+  }
+}
