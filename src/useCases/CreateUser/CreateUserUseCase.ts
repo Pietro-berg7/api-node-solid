9b61b78864b9550714928204817ad40508274b74
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-04-03 04:01:10 +0044

    Finalizando useCase Create User

--- src/useCases/CreateUser/CreateUserUseCase.ts
+++ src/useCases/CreateUser/CreateUserUseCase.ts
@@ -1,9 +1,13 @@
 import { User } from "../../entities/User";
 import { ICreateUserRequestDTO } from "./CreateUserDTO";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { IMailProvider } from "../../providers/IMailProvider";
 
 export class CreateUserUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+  constructor(
+    private usersRepository: IUsersRepository,
+    private mailProvider: IMailProvider
+  ) {}
 
   async execute(data: ICreateUserRequestDTO) {
     const userAlreadyExists = await this.usersRepository.findByEmail(
@@ -17,5 +21,18 @@ export class CreateUserUseCase {
     const user = new User(data);
 
     await this.usersRepository.save(user);
+
+    this.mailProvider.sendMail({
+      to: {
+        name: data.name,
+        email: data.email,
+      },
+      from: {
+        name: "Equipe do Meu App",
+        email: "equipe@meuapp.com",
+      },
+      subject: "Seja bem vindo à plataforma!",
+      body: "<p>Você já pode fazer login em nossa plataforma.</p>",
+    });
   }
 }
